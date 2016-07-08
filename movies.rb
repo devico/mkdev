977d11a38b20fd9231671ed5adc5fe1d43d5b613
devico <clamdm@mail.ru>
2016-07-08 22:47:33 +0300

    Решение

--- movies.rb
+++ movies.rb
@@ -1,11 +1,12 @@
-name_film = ARGV[0]
+name_of_film = ARGV[0]
 film_bad = "Titanic"
 film_good = "Matrix"
 
-if name_film == film_bad
+if name_of_film == film_bad
   puts "#{film_bad} is a bad movie"
-elsif name_film == film_good
+elsif name_of_film == film_good
   puts "#{film_good} is a good movie"
 else 
   puts "Haven\'t seen #{name_film} yet"
 end
+
--- movies_array.rb
+++ movies_array.rb
@@ -1,8 +1,9 @@
-name_film = ARGV.first
+name = ARGV.first
 films = ["Titanic", "Matrix", "Revolver", "Equalizer"]
 
-if films.include?(name_film)
+if films.include?(name)
   puts "#{name_film} is a good movie"
 else 
   puts "#{name_film} is a bad movie"
 end
+
